3951c659e40b64d1a86bab1bec35afe86ae6f021
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-07 22:38:25 -0500

    working on catergory model

--- models/Category.js
+++ models/Category.js
@@ -12,6 +12,10 @@ Category.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
   },
   {
     sequelize,
