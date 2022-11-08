8d7deabaf2a56f037b5df4b8230ee98c01de7282
KaptSprinkles <KSoto18@users.noreply.github.com>
2022-11-07 23:21:02 -0500

    get all categories by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,12 +4,34 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
+
     try {
+      const allCategories = await Category.findAll({
+        include: [{ model: Product }]
+      })
+
+      res.status(200).json(allCategories);
+
+    }catch(err) {
+      console.info(err);
+      res.status(500).json(err);
+    }
+  
+});
+
+router.get('/:id', async (req, res) => {
+
+   try {
       const allCategories = await Category.findAll({
         include: [{ model: Product,
                     attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
                  }]
       })
+      
+      if (!allCategories) {
+        res.status(404).json({ message: 'Input a valid ID...'});
+        return;
+      }
       
       res.status(200).json(allCategories);
 
@@ -20,10 +42,6 @@ router.get('/', async (req, res) => {
   
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-});
 
 router.post('/', (req, res) => {
   // create a new category
